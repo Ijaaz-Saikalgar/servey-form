a7feb48347c6444c4abbdbae3bfb2db4043be985
Ijaaz-Saikalgar <ijaaz.saikalgar@gmail.com>
2025-05-09 20:30:13 +0530

    note for nexxt button fixed

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const nextBtn = document.getElementById("nextbtn");
 const prevBtn = document.getElementById("prevbtn");
+const note = document.getElementById("note");
 const container = document.getElementById("servey-container");
-const radioCheck = document.getElementById("rate");
+const radioChecks = document.querySelectorAll('input[name="ans"]');
 const questions = document.querySelectorAll(".question");
 const ans = document.getElementsByName("ans");
 
-
 var questionIndex = 0;
+var radioChecked = false;
 
 function fadeInOut() {
     container.className = 'servey-container fade'
-    setTimeout(function () { container.className = 'servey-container' }, 600);
+    setTimeout(function () { container.className = 'servey-container' }, 450);
 }
 
 // next btn 
 function nextQuestion() {
-    questionIndex++;
+    note.classList.remove("show-required");
+    ++questionIndex;
     if (questionIndex == 0) {
         questions[questionIndex].className = "question";
     } else if (questionIndex == 1) {
@@ -31,14 +33,17 @@ function nextQuestion() {
     } else if (questionIndex == 4) {
         questions[3].className = "question";
         questions[questionIndex].className = "question display-question";
+    } else if (questionIndex == 5) {
         nextBtn.className = "btn next-hide";
+        alert("Thank You!! The Servey has been recorded.")
     }
     console.log(questionIndex)
+
 }
 
 // prev btn
 function prevQuestion() {
-    questionIndex--;
+    --questionIndex;
     if (questionIndex == 0) {
         questions[1].className = "question";
         questions[questionIndex].className = "question display-question";
@@ -66,26 +71,27 @@ prevBtn.addEventListener("click", function () {
 })
 
 nextBtn.addEventListener("click", function () {
-    if (radioCheck.checked) {
-        console.log("checked")
-        fadeInOut();
-        // clearCheck()
+    radioChecks.forEach((radioCheck) => {
+        if (radioCheck.checked) {
+            radioChecked = true;
+            radioCheck.checked = false;
+        }
+    })
+
+    if (radioChecked) {
+        note.classList.remove("show-required");
+        if (questionIndex <= 3) {
+            fadeInOut();
+        } else null;
         setTimeout(function () {
             nextQuestion();
-        }, 500)
-    } else {
-        console.log("unchecked")
+        }, 400)
     }
-})
+    else {
+        note.className = "show-required";
+    }
+    radioChecked = false;
+});
 
-
-
-
-// function clearCheck() {
-//     ans.forEach(element => {
-//         element.checked = false;
-//         console.log(element)
-//     })
-// }
 
 
