4e82b9ce3bb8a93436b8cbdc63719e9ae7d5acd5
Ijaaz-Saikalgar <ijaaz.saikalgar@gmail.com>
2025-05-13 01:23:53 +0530

    old ans marks check updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const nextBtn = document.getElementById("nextbtn");
 const prevBtn = document.getElementById("prevbtn");
 const note = document.getElementById("note");
@@ -6,8 +7,9 @@ const radioChecks = document.querySelectorAll('input[name="ans"]');
 const questions = document.querySelectorAll(".question");
 const ans = document.getElementsByName("ans");
 
-var questionIndex = 0;
-var radioChecked = false;
+let questionIndex = 0;
+let checkedCondition = false;
+let radioRecordes = [];
 
 function fadeInOut() {
     container.className = 'servey-container fade'
@@ -18,80 +20,90 @@ function fadeInOut() {
 function nextQuestion() {
     note.classList.remove("show-required");
     ++questionIndex;
-    if (questionIndex == 0) {
+
+    if (questionIndex === 0) {
         questions[questionIndex].className = "question";
-    } else if (questionIndex == 1) {
-        questions[0].className = "question";
+    } else if (questionIndex <= 4) {
+        questions[questionIndex - 1].className = "question";
         questions[questionIndex].className = "question display-question";
-        prevBtn.className = "btn";
-    } else if (questionIndex == 2) {
-        questions[1].className = "question";
-        questions[questionIndex].className = "question display-question";
-    } else if (questionIndex == 3) {
-        questions[2].className = "question";
-        questions[questionIndex].className = "question display-question";
-    } else if (questionIndex == 4) {
-        questions[3].className = "question";
-        questions[questionIndex].className = "question display-question";
-    } else if (questionIndex == 5) {
+        if (questionIndex === 1) prevBtn.className = "btn";
+    } else if (questionIndex === 5) {
         nextBtn.className = "btn next-hide";
-        alert("Thank You!! The Servey has been recorded.")
+        alert("Thank You!! The Survey has been recorded.");
     }
-    console.log(questionIndex)
 
 }
 
 // prev btn
 function prevQuestion() {
     --questionIndex;
-    if (questionIndex == 0) {
-        questions[1].className = "question";
-        questions[questionIndex].className = "question display-question";
-        prevBtn.className = "btn prev-hide";
-    } else if (questionIndex == 1) {
-        questions[2].className = "question";
-        questions[questionIndex].className = "question display-question";
-    } else if (questionIndex == 2) {
-        questions[3].className = "question";
-        questions[questionIndex].className = "question display-question";
-    } else if (questionIndex == 3) {
-        questions[4].className = "question";
-        questions[questionIndex].className = "question display-question";
-        nextBtn.className = "btn ";
+    switch (questionIndex) {
+        case 0:
+            questions[1].className = "question";
+            questions[questionIndex].className = "question display-question";
+            prevBtn.className = "btn prev-hide";
+            break;
+        case 1:
+            questions[2].className = "question";
+            questions[questionIndex].className = "question display-question";
+            break;
+        case 2:
+            questions[3].className = "question";
+            questions[questionIndex].className = "question display-question";
+            break;
+        case 3:
+            questions[4].className = "question";
+            questions[questionIndex].className = "question display-question";
+            nextBtn.className = "btn ";
+            break;
     }
-    console.log(questionIndex)
 }
 
 prevBtn.addEventListener("click", function () {
     fadeInOut();
-
     setTimeout(function () {
         prevQuestion();
+
+        const selectedIndex = radioRecordes[questionIndex] - 1;
+        if (selectedIndex >= 0) {
+            radioChecks[selectedIndex].checked = true;
+        }
     }, 500)
+
 })
 
-nextBtn.addEventListener("click", function () {
+function radioCheackUpdate(questionIndex) {
     radioChecks.forEach((radioCheck) => {
+
         if (radioCheck.checked) {
-            radioChecked = true;
+            radioRecordes[questionIndex] = +radioCheck.id.slice(1);
+            checkedCondition = true;
             radioCheck.checked = false;
         }
     })
+}
 
-    if (radioChecked) {
+
+nextBtn.addEventListener("click", function () {
+    radioCheackUpdate(questionIndex);
+
+    if (checkedCondition) {
         note.classList.remove("show-required");
+
         if (questionIndex <= 3) {
             fadeInOut();
-        } else null;
+        }
+
         setTimeout(function () {
             nextQuestion();
-        }, 400)
-    }
-    else {
+            const selectedIndex = radioRecordes[questionIndex] - 1;
+            radioChecks[selectedIndex].checked = true;
+        }, 400);
+    } else {
         note.className = "show-required";
     }
-    radioChecked = false;
-});
 
+    checkedCondition = false;
+});
 
 
